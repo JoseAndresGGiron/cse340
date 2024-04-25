92c803fb9d81f4e612e5d94cba4e56bf81b70125
JoseAndresGGiron <titogg@byui.edu>
2024-04-24 21:57:08 -0400

    Changes done as described on EJS View Engine instructions.  Note index.ejs appears only in the image

--- server.js
+++ server.js
@@ -6,6 +6,7 @@
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
@@ -13,6 +14,14 @@ const static = require("./routes/static")
 /* ***********************
  * Routes
  *************************/
+
+/* ***********************
+ * View Engine and Templates
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
+
 app.use(static)
 
 /* ***********************

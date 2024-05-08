d8311a1e604b30856498eda6807cc672e2224057
JoseAndresGGiron <titogg@byui.edu>
2024-05-07 20:39:01 -0400

    adding w3 learning activities

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 const baseController = require("./controllers/baseController") // Added require statement for baseController
+const utilities = require("./utilities/"); //Adeded require statment on w3 activities so line 41 works good
 
 /* ***********************
  * View Engine and Templates
@@ -24,7 +25,14 @@ app.use(static)
 /* ***********************
  * Routes
  *************************/
-app.get("/", baseController.buildHome) // Altered route to call baseController method
+
+// Index route
+app.get("/", utilities.handleErrors(baseController.buildHome))
+
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+})
 
 /* Before line looked like this:
 app.get("/", function(req, res){
@@ -32,6 +40,20 @@ app.get("/", function(req, res){
 })
 */
 
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message,
+    nav
+  })
+})
 
 /* ***********************
  * Local Server Information

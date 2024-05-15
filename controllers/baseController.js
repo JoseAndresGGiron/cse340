575f6d17680b6e7c0ccf7c16f222e715408ee76a
JoseAndresGGiron <titogg@byui.edu>
2024-05-14 21:05:45 -0400

    session and messages activity completed

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -3,6 +3,8 @@ const baseController = {}
 
 baseController.buildHome = async function(req, res){
   const nav = await utilities.getNav()
+  //The below line was added and commented out as part of w4 learning activitites. The "notice" adds a class to the flash messages, I can write css rules usint these classes.
+  //req.flash("notice", "This is a flash message.")
   res.render("index", {title: "Home", nav})
 }
 
--- server.js
+++ server.js
@@ -5,6 +5,10 @@
 /* ***********************
  * Require Statements
  *************************/
+//W4 learning Activity:
+const session = require("express-session")
+const pool = require('./database/')
+
 const express = require("express")
 const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
@@ -14,6 +18,27 @@ const baseController = require("./controllers/baseController") // Added require 
 const utilities = require("./utilities/"); //Adeded require statment on w3 activities so line 41 works good
 //w3 inventory classification activity
 const inventoryRoute = require("./routes/inventoryRoute")
+
+/* ***********************
+ * Middleware
+ * ************************/
+app.use(session({
+  store: new (require('connect-pg-simple')(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId',
+}))
+
+// Express Messages Middleware from W4 learning Activities
+app.use(require('connect-flash')())
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
 
 /* ***********************
  * View Engine and Templates

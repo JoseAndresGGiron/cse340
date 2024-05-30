9ba24daed0ee65cd8b8eb8b3f664dcec5a2e9420
JoseAndresGGiron <titogg@byui.edu>
2024-05-30 19:44:01 -0400

    reviews  table created

--- database/db-sql-code.sql
+++ database/db-sql-code.sql
@@ -258,3 +258,14 @@ example: /images/vehicles/a-car-name.jpg
 UPDATE public.inventory
 SET inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
     inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
+
+/*Code to create Review tables*/
+CREATE TABLE reviews (
+    review_id SERIAL PRIMARY KEY,
+    review_text TEXT NOT NULL,
+    review_date TIMESTAMP WITH TIME ZONE DEFAULT NOW() NOT NULL,
+    inv_id INTEGER NOT NULL,
+    account_id INTEGER NOT NULL,
+    FOREIGN KEY (inv_id) REFERENCES inventory(inv_id),
+    FOREIGN KEY (account_id) REFERENCES account(account_id)
+);
